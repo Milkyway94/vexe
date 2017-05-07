c63f62a13b5cbf310dfbc4a3eaba221f7366f05e
Hồ Sỹ Quyết <hoquyet.1994@gmail.com>
2017-05-08 01:56:23 +0700

    somechange

--- commit.bat
+++ commit.bat
@@ -1,13 +1,13 @@
-echo "Add to master"
+echo "****** Them cac file thay doi******"
 git add *
-echo Commit some change on 
+echo ****** Commit len master
 
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
-echo %mydate%_%mytime%
+echo ****** Ngày %mydate% %mytime%
 git commit -m somechange
-echo PULL to origin
+echo ******PULL ve origin*****
 git pull
-echo Push to master
+echo ******Push len master******
 git push
