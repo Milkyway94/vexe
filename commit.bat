c49e9b15b3a6df899861ed7a26b8af05df6d97e8
Hồ Sỹ Quyết <hoquyet.1994@gmail.com>
2017-05-08 01:53:02 +0700

    somechange

--- commit.bat
+++ commit.bat
@@ -1,3 +1,17 @@
+echo "Add to master"
 git add *
+echo Commit some change on 
+
+down vote
+accepted
+See Windows Batch File (.bat) to get current date in MMDDYYYY format.:
+
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
 git commit -m somechange
+echo PULL to origin
+git pull
+echo Push to master
 git push
